0a32b4e5a3253ee29aac97ff6ae4080caf2d1c0b
Scott Fillmer <scottfillmer@me.com>
2022-04-15 13:51:20 -0500

    first commit

---
+++ kelvin.js
@@ -0,0 +1,11 @@
+/* this was the project based in codecademy that I have redone, and worked great this time, the second time around
+
+*/
+
+const kelvin = 0; // this is the kelvin variable
+const celsius = kelvin - 273; // convert
+let fahrenheit = celsius * (9/5) + 32;
+
+fahrenheit = Math.floor(fahrenheit); // convert
+console.log('The temperature is ' + fahrenheit + ' degrees Fahrenheit.');
+
